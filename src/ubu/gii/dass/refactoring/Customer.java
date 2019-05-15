5fb01182ee1d3f9bb4eab493887b5bb8ea5bd43f
Javier García Gonzalez <jgg0176@alu.ubu.es>
2019-05-15 19:57:35 +0200

    Move Method frequentRenterPoints
    Se mueve el método de la clase Customer a Rental

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -55,14 +55,4 @@ public class Customer {
 		return result;
 	}
 	
-	private int frequentRenterPoints(int frequentRenterPoints, Rental each) {
-		// add frequent renter points
-		frequentRenterPoints++;
-		// add bonus for a two day new release rental
-		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-				&& each.getDaysRented() > 1)
-			frequentRenterPoints++;
-		return frequentRenterPoints;
-	}
-	
 }
